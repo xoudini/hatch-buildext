1ce1a94c4e97f78e9e8a84b6353e8b3d34feb979
Dan Lindholm <git@dan.mk>
2024-12-16 23:46:02 +0100

    expose hook and plugin

--- src/hatch_buildext/__init__.py
+++ src/hatch_buildext/__init__.py
@@ -1,3 +1,13 @@
 # SPDX-FileCopyrightText: 2024-present tunnelworks <git@tunnelworks.org>
 #
 # SPDX-License-Identifier: Apache-2.0
+
+
+from hatch_buildext.hooks import hatch_register_build_hook
+from hatch_buildext.plugin import ExtensionBuildHook
+
+
+__all__ = (
+    "hatch_register_build_hook",
+    "ExtensionBuildHook",
+)
