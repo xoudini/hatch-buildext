bcdb95cd368c101be3f457f81d9c4b476f96b698
Dan Lindholm <git@dan.mk>
2024-12-18 21:52:55 +0100

    clean up plugin

--- src/hatch_buildext/plugin.py
+++ src/hatch_buildext/plugin.py
@@ -9,46 +9,16 @@ import shutil
 import tempfile
 import typing as t
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+from hatch_buildext.resolver import Resolver
 
 
 if t.TYPE_CHECKING:
-    from types import ModuleType
     from hatch_buildext._types import BuildData
 
 
 class _Extension(t.NamedTuple):
     name: str
     resolver: str
-
-
-@t.runtime_checkable
-class _GetSources(t.Protocol):
-    def get_sources(self, root: str) -> t.Sequence[str]: ...
-
-
-@t.runtime_checkable
-class _GetIncludeDirs(t.Protocol):
-    def get_include_dirs(self, root: str) -> t.Sequence[str]: ...
-
-
-@t.runtime_checkable
-class _GetLibraryDirs(t.Protocol):
-    def get_library_dirs(self, root: str) -> t.Sequence[str]: ...
-
-
-@t.runtime_checkable
-class _GetLibraries(t.Protocol):
-    def get_libraries(self, root: str) -> t.Sequence[str]: ...
-
-
-@t.runtime_checkable
-class _GetExtraCompileArgs(t.Protocol):
-    def get_extra_compile_args(self, root: str) -> t.Sequence[str]: ...
-
-
-@t.runtime_checkable
-class _GetExtraLinkArgs(t.Protocol):
-    def get_extra_link_args(self, root: str) -> t.Sequence[str]: ...
 
 
 class ExtensionBuildHook(BuildHookInterface):
@@ -106,43 +76,13 @@ class ExtensionBuildHook(BuildHookInterface):
 
         return self.__extensions
 
-    def _load(self, resolver: str) -> "ModuleType":
+    def _get_resolver(self, name: str) -> Resolver:
         from hatch_buildext._utils import add_to_sys_path
 
         with add_to_sys_path(path=self.root):
-            module = importlib.import_module(resolver)
+            module = importlib.import_module(name)
 
-        return module
-
-    def _get_sources(self, module: "ModuleType") -> t.List[str]:
-        if isinstance(module, _GetSources):
-            return module.get_sources(root=self.root)
-        return []
-
-    def _get_include_dirs(self, module: "ModuleType") -> t.List[str]:
-        if isinstance(module, _GetIncludeDirs):
-            return module.get_include_dirs(root=self.root)
-        return []
-
-    def _get_library_dirs(self, module: "ModuleType") -> t.List[str]:
-        if isinstance(module, _GetLibraryDirs):
-            return module.get_library_dirs(root=self.root)
-        return []
-
-    def _get_libraries(self, module: "ModuleType") -> t.List[str]:
-        if isinstance(module, _GetLibraries):
-            return module.get_libraries(root=self.root)
-        return []
-
-    def _get_extra_compile_args(self, module: "ModuleType") -> t.List[str]:
-        if isinstance(module, _GetExtraCompileArgs):
-            return module.get_extra_compile_args(root=self.root)
-        return []
-
-    def _get_extra_link_args(self, module: "ModuleType") -> t.List[str]:
-        if isinstance(module, _GetExtraLinkArgs):
-            return module.get_extra_link_args(root=self.root)
-        return []
+        return Resolver(root=self.root, module=module)
 
     def _build(self) -> None:
         from setuptools import Distribution, Extension
@@ -155,23 +95,17 @@ class ExtensionBuildHook(BuildHookInterface):
             _extensions: t.List["Extension"] = list()
 
             for extension in self._extensions:
-                module = self._load(resolver=extension.resolver)
-                _sources = self._get_sources(module=module)
-                _include_dirs = self._get_include_dirs(module=module)
-                _library_dirs = self._get_library_dirs(module=module)
-                _libraries = self._get_libraries(module=module)
-                _extra_compile_args = self._get_extra_compile_args(module=module)
-                _extra_link_args = self._get_extra_link_args(module=module)
+                resolver = self._get_resolver(name=extension.resolver)
 
                 _extension = Extension(
                     name=extension.name,
-                    sources=_sources,
-                    include_dirs=_include_dirs,
-                    library_dirs=_library_dirs,
-                    libraries=_libraries,
+                    sources=resolver.sources,
+                    include_dirs=resolver.include_dirs,
+                    library_dirs=resolver.library_dirs,
+                    libraries=resolver.libraries,
                     runtime_library_dirs=None,
-                    extra_compile_args=_extra_compile_args,
-                    extra_link_args=_extra_link_args,
+                    extra_compile_args=resolver.extra_compile_args,
+                    extra_link_args=resolver.extra_link_args,
                 )
                 _extensions.append(_extension)
 
