ce48c849fc639d207ac4d4496a9a307d5194a4ab
Dan Lindholm <git@dan.mk>
2024-12-18 21:54:34 +0100

    wrap types in type checking block

--- src/hatch_buildext/_types.py
+++ src/hatch_buildext/_types.py
@@ -1,8 +1,10 @@
 import typing as t
 
 
-class BuildData(t.TypedDict):
-    infer_tag: bool
-    pure_python: bool
-    artifacts: t.List[str]
-    force_include: t.Dict[str, str]
+if t.TYPE_CHECKING:
+
+    class BuildData(t.TypedDict):
+        infer_tag: bool
+        pure_python: bool
+        artifacts: t.List[str]
+        force_include: t.Dict[str, str]
--- tests/_types.py
+++ tests/_types.py
@@ -1,61 +1,52 @@
 import typing as t
 
 
-_Builder = t.Literal["hatchling"]
-_BuildBackend = t.Literal["hatchling.build"]
+if t.TYPE_CHECKING:
+    _Builder = t.Literal["hatchling"]
+    _BuildBackend = t.Literal["hatchling.build"]
 
-_BuildSystemConfig = t.TypedDict(
-    "_BuildSystemConfig",
-    {
-        "requires": t.Sequence[_Builder],
-        "build-backend": _BuildBackend,
-    },
-)
+    _BuildSystemConfig = t.TypedDict(
+        "_BuildSystemConfig",
+        {
+            "requires": t.Sequence[_Builder],
+            "build-backend": _BuildBackend,
+        },
+    )
 
+    class _ProjectConfig(t.TypedDict):
+        name: str
+        version: str
 
-class _ProjectConfig(t.TypedDict):
-    name: str
-    version: str
+    class _BuildextHookConfig(t.TypedDict):
+        dependencies: t.Sequence[t.Literal["hatch-buildext"]]
+        extensions: t.Mapping[str, str]
 
+    class _HooksConfig(t.TypedDict):
+        buildext: _BuildextHookConfig
 
-class _BuildextHookConfig(t.TypedDict):
-    dependencies: t.Sequence[t.Literal["hatch-buildext"]]
-    extensions: t.Mapping[str, str]
+    class _WheelConfig(t.TypedDict):
+        packages: t.Sequence[str]
+        hooks: _HooksConfig
 
+    class _TargetConfig(t.TypedDict):
+        wheel: _WheelConfig
 
-class _HooksConfig(t.TypedDict):
-    buildext: _BuildextHookConfig
+    class _BuildConfig(t.TypedDict):
+        targets: _TargetConfig
 
+    class _HatchConfig(t.TypedDict):
+        build: _BuildConfig
 
-class _WheelConfig(t.TypedDict):
-    packages: t.Sequence[str]
-    hooks: _HooksConfig
+    class _ToolConfig(t.TypedDict):
+        hatch: _HatchConfig
 
+    PyProjectConfig = t.TypedDict(
+        "PyProjectConfig",
+        {
+            "build-system": _BuildSystemConfig,
+            "project": _ProjectConfig,
+            "tool": _ToolConfig,
+        },
+    )
 
-class _TargetConfig(t.TypedDict):
-    wheel: _WheelConfig
-
-
-class _BuildConfig(t.TypedDict):
-    targets: _TargetConfig
-
-
-class _HatchConfig(t.TypedDict):
-    build: _BuildConfig
-
-
-class _ToolConfig(t.TypedDict):
-    hatch: _HatchConfig
-
-
-PyProjectConfig = t.TypedDict(
-    "PyProjectConfig",
-    {
-        "build-system": _BuildSystemConfig,
-        "project": _ProjectConfig,
-        "tool": _ToolConfig,
-    },
-)
-
-
-_DistFile = t.Literal["RECORD", "METADATA", "WHEEL"]
+    _DistFile = t.Literal["RECORD", "METADATA", "WHEEL"]
