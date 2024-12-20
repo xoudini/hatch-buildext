ce5d2c919d1a0da74d188b2882a9a8fcf6470742
Dan Lindholm <git@dan.mk>
2024-12-20 14:09:28 +0200

    fix typing in test utils

--- tests/_utils.py
+++ tests/_utils.py
@@ -3,8 +3,10 @@ from hatch_buildext import hooks as _hooks
 
 
 if t.TYPE_CHECKING:
-    from hatchling.builders.plugin.interface import BuilderInterface
+    from hatchling.builders.config import BuilderConfigBound as Config
+    from hatchling.builders.plugin.interface import BuilderInterface as Builder
+    from hatchling.plugin.manager import PluginManagerBound as PluginManager
 
 
-def register_hooks(builder: "BuilderInterface") -> None:
+def register_hooks(builder: "Builder[Config, PluginManager]") -> None:
     builder.plugin_manager.manager.register(_hooks)
