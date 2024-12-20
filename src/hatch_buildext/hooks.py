f7f7cec8ed5c85bac0cfd026a9d8ebac6f0971d0
Dan Lindholm <git@dan.mk>
2024-12-20 14:04:59 +0200

    add type annotation to hook registration

--- src/hatch_buildext/hooks.py
+++ src/hatch_buildext/hooks.py
@@ -1,7 +1,8 @@
+import typing as t
 from hatchling.plugin import hookimpl
 from hatch_buildext.plugin import ExtensionBuildHook
 
 
 @hookimpl
-def hatch_register_build_hook():
+def hatch_register_build_hook() -> t.Type[ExtensionBuildHook]:
     return ExtensionBuildHook
